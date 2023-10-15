6f4ed12eff99f08c8a6e9068adecd105d37f354f
Joakim Tviberg Fengås <117069660+jotvibfeng@users.noreply.github.com>
2023-10-15 17:36:17 +0200

    CA delivery

---
+++ script.js/script.js
@@ -0,0 +1,22 @@
+var winningNumbers = [1, 76, 2, 78, 16, 7];
+
+var result = winningNumbers.find (function (number) {
+    return number === 2 ;
+});
+
+console.log(result);
+
+var vegetables = ["rice", "potatoes", "porridge", "cake", "spinach"];
+
+vegetables.push("salt");
+
+console.log(vegetables);
+
+var randomThings = ["pumpkin", false, 23, true, "tomato"];
+
+for (var i = 0; i < randomThings.length; i++) {
+    console.log(randomThings[i]);
+  }
+
+  var moreThings = [14, "cat", true, 29, "fox", true, 34, 19];
+
